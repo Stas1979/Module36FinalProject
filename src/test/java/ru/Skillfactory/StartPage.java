25516e21561c1959098dc7414493c31f2d2571dd
Станислав <117911913+Stas1979@users.noreply.github.com>
2023-06-05 13:53:00 +0300

    Update StartPage.java

--- src/test/java/ru/Skillfactory/StartPage.java
+++ src/test/java/ru/Skillfactory/StartPage.java
@@ -1,4 +1,4 @@
-package FinalProject;
+package ru.Skillfactory;
 
 
 import org.openqa.selenium.By;
@@ -13,29 +13,74 @@ public class StartPage {
     }
 
     private static By dataScience = By.cssSelector("#rec562561178 > div > div > div.t396__elem.tn-elem.tn-elem__5625611781679404657898.js-sbs-anim-trigger_hover > a");
-    private static By dataAnalyst = By.cssSelector("#rec562561178 > div > div > div.t396__elem.tn-elem.tn-elem__5625611781679404895615.js-sbs-anim-trigger_hover > a");
+    private static By dataAnalyst = By.cssSelector("#rec562561178 > div > div > div.t396__elem.tn-elem.tn-elem__5625611781679404895615.js-sbs-anim-trigger_hover");
     private static By testing = By.cssSelector("#rec562561178 > div > div > div.t396__elem.tn-elem.tn-elem__5625611781679405234205.js-sbs-anim-trigger_hover > a");
     private static By higherEducation = By.cssSelector("#rec562561178 > div > div > div.t396__elem.tn-elem.tn-elem__5625611781679405356847.js-sbs-anim-trigger_hover > a");
+    private static By onlineCourses = By.cssSelector("#sbs-562561178-1678713806317 > div > a");
+    private static By forFree = By.cssSelector("#sbs-562561178-1678713806343 > div > a");
+    private static By careerCenter = By.cssSelector("#sbs-562561178-1678713806322 > div > a");
+    private static By contacts = By.cssSelector("#sbs-562561178-1678713806326 > div > a");
+    private static By media = By.cssSelector("#sbs-562561178-1678713806329 > div > a");
+    private static By corporateTraining = By.cssSelector("#sbs-562561178-1678713806333 > div > a");
+
 
     public void startOpen1() {
         driver.get("https://skillfactory.ru/#submenu:details");
-
     }
 
     public void startClick() {
-
-        Boolean visibleState = driver.findElement(dataScience).isDisplayed();
-        System.out.println("The Submit Button visibility is: " + visibleState);
-
-        if (visibleState) {
-            Boolean status = driver.findElement(dataScience).isEnabled();
-            System.out.println("The status of Submit Button is: " + status);
-
-            if (status) {
-                driver.findElement(dataScience).click();
-                //Data Science
-            }
-
-        }
+        driver.findElement(dataScience).click();
+        //Data Science
     }
+
+    public void startClick1() {
+        driver.findElement(dataAnalyst).click();
+        //Аналитик данных
+    }
+
+    public void startClick2() {
+        driver.findElement(testing).click();
+        //Тестирование
+    }
+
+    public void startClick3() {
+        driver.findElement(higherEducation).click();
+        //Высшее образование
+    }
+
+    public void headerClick() {
+        driver.findElement(onlineCourses).click();
+        //Онлайн-курсы
+
+    }
+
+    public void headerClick1() {
+        driver.findElement(forFree).click();
+        //Бесплатно
+
+    }
+
+    public void headerClick2() {
+        driver.findElement(careerCenter).click();
+        //Центр карьеры
+
+    }
+
+    public void headerClick3() {
+        driver.findElement(contacts).click();
+        //Контакты
+
+    }
+
+    public void headerClick4() {
+        driver.findElement(media).click();
+        //Медиа
+
+    }
+
+    public void headerClick5() {
+        driver.findElement(corporateTraining).click();
+        //Корпоративное обучение
+    }
+
 }
