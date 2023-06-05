60d01ba7866ccb50b36865f6dd86c4f6462ef7ef
Станислав <117911913+Stas1979@users.noreply.github.com>
2023-06-05 13:49:19 +0300

    Update WebDriverSettings.java

--- src/test/java/ru/Skillfactory/WebDriverSettings.java
+++ src/test/java/ru/Skillfactory/WebDriverSettings.java
@@ -1,2 +1,23 @@
-package ru.Skillfactory;public class WebDriverSettings {
+package ru.Skillfactory;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverSettings {
+
+    public ChromeDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Java\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void close() {
+        driver.quit();
+
+    }
+
 }
